47688541d37fb90f5812f805bcc3b421ec71f412
Will Pankiewicz <will.pankiewicz@gmail.com>
2016-03-28 01:53:59 -0500

    first commit

---
+++ src/neuralNetwork/NeuralNetTest.java
@@ -0,0 +1,12 @@
+package neuralNetwork;
+
+public class NeuralNetTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NeuralNet n  = new NeuralNet();
+		n.initNet();
+		n.printNet();
+	}
+
+}
